db9019e174139dcf6c5f4a00cac5a049773423fb
Tyler Epstein <tylerepstein27@gmail.com>
2021-04-29 15:14:22 -0400

    adds bootstrap theme

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,167 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/signup">Sign Up</router-link>
-      |
-      <router-link v-if="id === null" to="/login">Log In</router-link>
-      |
-      <router-link v-if="id !== null" to="/logout">Log Out</router-link>
-      |
-      <router-link v-if="id !== null" v-bind:to="`/userlist/${this.id}`">My List</router-link>
-    </div>
+    <!-- Header section -->
+    <header class="header-section">
+      <div class="container">
+        <!-- logo -->
+        <a class="site-logo" href="/">
+          <img src="img/logo.png" alt="" />
+        </a>
+        <!-- add conditional here to render username/logout option vs login -->
+        <div class="user-panel">
+          <a href="/login">Login</a>
+          /
+          <a href="/signup">Register</a>
+        </div>
+        <!-- responsive -->
+        <div class="nav-switch">
+          <i class="fa fa-bars"></i>
+        </div>
+        <!-- site menu -->
+        <nav class="main-menu">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><router-link v-bind:to="`/userlist/${id}`">My List</router-link></li>
+            <!-- <li><a href="review.html">Games</a></li>
+            <li><a href="categories.html">Blog</a></li>
+            <li><a href="community.html">Forums</a></li>
+            <li><a href="contact.html">Contact</a></li> -->
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <!-- Header section end -->
     <router-view />
+
+    <!-- Footer top section -->
+    <!-- <section class="footer-top-section">
+      <div class="container">
+        <div class="footer-top-bg">
+          <img src="img/footer-top-bg.png" alt="" />
+        </div>
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="footer-logo text-white">
+              <img src="img/footer-logo.png" alt="" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor sit ame.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-widget mb-5 mb-md-0">
+              <h4 class="fw-title">Latest Posts</h4>
+              <div class="latest-blog">
+                <div class="lb-item">
+                  <div class="lb-thumb set-bg" data-setbg="img/latest-blog/1.jpg"></div>
+                  <div class="lb-content">
+                    <div class="lb-date">June 21, 2018</div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum</p>
+                    <a href="#" class="lb-author">By Admin</a>
+                  </div>
+                </div>
+                <div class="lb-item">
+                  <div class="lb-thumb set-bg" data-setbg="img/latest-blog/2.jpg"></div>
+                  <div class="lb-content">
+                    <div class="lb-date">June 21, 2018</div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum</p>
+                    <a href="#" class="lb-author">By Admin</a>
+                  </div>
+                </div>
+                <div class="lb-item">
+                  <div class="lb-thumb set-bg" data-setbg="img/latest-blog/3.jpg"></div>
+                  <div class="lb-content">
+                    <div class="lb-date">June 21, 2018</div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum</p>
+                    <a href="#" class="lb-author">By Admin</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-widget">
+              <h4 class="fw-title">Top Comments</h4>
+              <div class="top-comment">
+                <div class="tc-item">
+                  <div class="tc-thumb set-bg" data-setbg="img/authors/1.jpg"></div>
+                  <div class="tc-content">
+                    <p>
+                      <a href="#">James Smith</a>
+                      <span>on</span>
+                      Lorem ipsum dolor sit amet, co
+                    </p>
+                    <div class="tc-date">June 21, 2018</div>
+                  </div>
+                </div>
+                <div class="tc-item">
+                  <div class="tc-thumb set-bg" data-setbg="img/authors/2.jpg"></div>
+                  <div class="tc-content">
+                    <p>
+                      <a href="#">James Smith</a>
+                      <span>on</span>
+                      Lorem ipsum dolor sit amet, co
+                    </p>
+                    <div class="tc-date">June 21, 2018</div>
+                  </div>
+                </div>
+                <div class="tc-item">
+                  <div class="tc-thumb set-bg" data-setbg="img/authors/3.jpg"></div>
+                  <div class="tc-content">
+                    <p>
+                      <a href="#">James Smith</a>
+                      <span>on</span>
+                      Lorem ipsum dolor sit amet, co
+                    </p>
+                    <div class="tc-date">June 21, 2018</div>
+                  </div>
+                </div>
+                <div class="tc-item">
+                  <div class="tc-thumb set-bg" data-setbg="img/authors/4.jpg"></div>
+                  <div class="tc-content">
+                    <p>
+                      <a href="#">James Smith</a>
+                      <span>on</span>
+                      Lorem ipsum dolor sit amet, co
+                    </p>
+                    <div class="tc-date">June 21, 2018</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> -->
+    <!-- Footer top section end -->
+
+    <!-- Footer section -->
+    <footer class="footer-section">
+      <div class="container">
+        <ul class="footer-menu">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="review.html">Games</a></li>
+          <li><a href="categories.html">Blog</a></li>
+          <li><a href="community.html">Forums</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+        <p class="copyright">
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          Copyright &copy;
+          <!-- <script>
+            document.write(new Date().getFullYear());
+          </script> -->
+          All rights reserved | This template is made with
+          <i class="fa fa-heart-o" aria-hidden="true"></i>
+          by
+          <a href="https://colorlib.com" target="_blank">Colorlib</a>
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        </p>
+      </div>
+    </footer>
+    <!-- Footer section end -->
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 
 <script>
 export default {
